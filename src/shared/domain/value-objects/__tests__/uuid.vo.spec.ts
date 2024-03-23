bcf4a0d0788f6ca254be96c657f82a5ca6dde40a
caioverissimo <caio.veb@gmail.com>
2024-03-23 12:50:35 -0300

    chore: adds uuid and @types/uuid pkgs; init 'Uuid' Value Object

---
+++ src/shared/domain/value-objects/__tests__/uuid.vo.spec.ts
@@ -0,0 +1,32 @@
+import { InvalidUuidError, Uuid } from "../uuid.vo";
+import { validate as uuidValidate } from 'uuid';
+
+describe('Uuid | Unit Tests', () => {
+
+  const validateSpy = jest.spyOn(Uuid.prototype as any, 'validate');
+
+  it('should throw error when uuid is invalid', () => {
+    const uuidGeneration = () => {
+      new Uuid('invalid-uuid')
+    };
+
+    expect(uuidGeneration).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should create a valid uuid', () => {
+    const uuid = new Uuid();
+
+    expect(uuid).toBeDefined();
+    expect(uuid.id).toBeDefined();
+    expect(uuidValidate(uuid.id)).toBe(true);
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  })
+
+  it('should accept a valid uuid', () => {
+    const uuid = new Uuid('d36bd018-d6c3-4119-8d79-6c1297263eda');
+
+    expect(uuid.id).toBe('d36bd018-d6c3-4119-8d79-6c1297263eda');
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  })
+});
