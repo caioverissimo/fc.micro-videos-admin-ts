19329c991f920d963af23fff2906699a80386526
caioverissimo <caio.veb@gmail.com>
2024-06-09 13:05:53 -0300

    feat(InMemoryRepository): applies custom 'entity' NotFoundError

--- src/shared/db/in-memory/in-memory.repository.ts
+++ src/shared/db/in-memory/in-memory.repository.ts
@@ -1,4 +1,5 @@
 import { Entity } from "../../domain/entity";
+import { NotFoundError } from "../../domain/errors/not-found.error";
 import { IRepository } from "../../domain/repository/repository-interface";
 import { ValueObject } from "../../domain/value-object";
 
@@ -19,7 +20,8 @@ export abstract class InMemoryRepository<E extends Entity, EntityId extends Valu
     const indexFound = this.items.findIndex((item) => item.entity_id.equals(entity.entity_id));
 
     if (indexFound === -1) {
-      throw new Error("Entity not found");
+      // throw new Error("Entity not found");
+      throw new NotFoundError(entity.entity_id, this.getEntity());
     }
 
     this.items[indexFound] = entity;
@@ -31,7 +33,8 @@ export abstract class InMemoryRepository<E extends Entity, EntityId extends Valu
     const indexFound = this.items.findIndex((item) => item.entity_id.equals(entity_id));
 
     if (indexFound === -1) {
-      throw new Error("Entity not found");
+      // throw new Error("Entity not found");
+      throw new NotFoundError(entity_id, this.getEntity());
     }
 
     this.items.splice(indexFound, 1);
