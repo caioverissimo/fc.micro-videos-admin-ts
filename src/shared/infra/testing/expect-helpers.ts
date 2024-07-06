1349d5aae014f52331b1feb87cb35d1a12cb32eb
caioverissimo <caio.veb@gmail.com>
2024-07-06 15:28:56 -0300

    chore: comment console.log's

--- src/shared/infra/testing/expect-helpers.ts
+++ src/shared/infra/testing/expect-helpers.ts
@@ -62,10 +62,10 @@ function assertContainsErrorsMessages(
 ) {
   const isMatch = expect.objectContaining(received).asymmetricMatch(expected);
 
-  if (received.name?.length === 2) {
-    console.log('@assertContainsErrorsMessages');
-    console.log('isMatch: ', isMatch);
-  }
+  // if (received.name?.length === 2) {
+  //   console.log('@assertContainsErrorsMessages');
+  //   console.log('isMatch: ', isMatch);
+  // }
 
   return isMatch
     ? isValid()
