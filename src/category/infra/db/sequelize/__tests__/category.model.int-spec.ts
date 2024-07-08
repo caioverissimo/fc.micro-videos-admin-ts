1dae17c7ddc6f6e0700eeae150878fa0061ec385
caioverissimo <caio.veb@gmail.com>
2024-07-08 11:30:37 -0300

    feat: init Config service; adds .env files; console.log Config() variables

--- src/category/infra/db/sequelize/__tests__/category.model.int-spec.ts
+++ src/category/infra/db/sequelize/__tests__/category.model.int-spec.ts
@@ -1,11 +1,13 @@
 import { DataType, Sequelize } from "sequelize-typescript";
 import { CategoryModel } from "../category.model";
 import { Category } from "../../../../domain/category.entity";
+import { Config } from "../../../../../shared/infra/config";
 
 describe('CategoryModel | Integration Tests', () => {
   let sequelize;
 
   beforeEach(async () => {
+    console.log('Config.db(): ', Config.db());
     sequelize = new Sequelize({
       dialect: "sqlite",
       storage: ":memory:",
